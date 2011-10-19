01da87dd0d284763fe13a53683dda317cc2094a9
Nat Budin <natbudin@gmail.com>
2011-10-19 15:32:19 -0400

    Redo the slug constraint a bit

--- lib/cadmus/routing.rb
+++ lib/cadmus/routing.rb
@@ -1,23 +1,30 @@
-class SlugConstraint
-	def self.matches?(request)
-		page_glob = request.params["page_glob"] || request.path
-		
-		page_glob.sub(/^\//, '').split(/\//).all? do |part|
-			part =~ /^[a-z][a-z0-9\-]*$/
+module Cadmus
+	class SlugConstraint
+		def matches?(request)
+			page_glob = request.symbolized_path_parameters[:page_glob]
+			
+			# assert_recognizes doesn't pass the full params hash as we would in a real Rails
+			# application.  So we have to always pass this constraint if we're testing.
+			return true if page_glob.nil? && Rails.env.test?
+			
+			page_glob.sub(/^\//, '').split(/\//).all? do |part|
+				part =~ /^[a-z][a-z0-9\-]*$/
+			end
 		end
 	end
 end
 
 ActionDispatch::Routing::Mapper.class_eval do
-	def cadmus_pages(controller)
-		get "pages" => "#{controller}#index"
+	def cadmus_pages(controller)	
+		get "pages" => "#{controller}#index", :as => 'pages'
 		get "pages/new" => "#{controller}#new", :as => 'new_page'
 		post "pages" => "#{controller}#create"
 
-	  get "*page_glob/edit" => "#{controller}#edit", :as => 'edit_page', :constraints => SlugConstraint
-  	get "*page_glob" => "#{controller}#show", :as => 'page', :constraints => SlugConstraint
-	  put "*page_glob" => "#{controller}#update", :constraints => SlugConstraint
-  	delete "*page_glob" => "#{controller}#destroy", :constraints => SlugConstraint
+		slug_constraint = Cadmus::SlugConstraint.new
+	  get "*page_glob/edit" => "#{controller}#edit", :as => 'edit_page', :constraints => slug_constraint
+  	get "*page_glob" => "#{controller}#show", :as => 'page', :constraints => slug_constraint
+	  put "*page_glob" => "#{controller}#update", :constraints => slug_constraint
+  	delete "*page_glob" => "#{controller}#destroy", :constraints => slug_constraint
 
 	end
 end
