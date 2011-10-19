ffccdac2da94da7b4a8f825500d4baf74b76d309
Nat Budin <natbudin@gmail.com>
2011-10-19 14:02:58 -0400

    Don't constrain the whole path, just page_glob

--- lib/cadmus/routing.rb
+++ lib/cadmus/routing.rb
@@ -1,6 +1,7 @@
 class SlugConstraint
 	def self.matches?(request)
-		request.path.sub(/^\//, '').split(/\//).all? do |part|
+		page_glob = request.params["page_glob"]
+		page_glob.sub(/^\//, '').split(/\//).all? do |part|
 			part =~ /^[a-z][a-z0-9\-]*$/
 		end
 	end
